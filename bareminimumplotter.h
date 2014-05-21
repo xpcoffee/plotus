41ef73a79c58989729fc6661ea34ae645805858d
Emerick Bosch <emerick.bosch@gmail.com>
2014-05-21 13:25:08 +0200

    initial commit - first working version of bare_minimum_plotter
    Signed-off-by: Emerick Bosch <emerick.bosch@gmail.com>

---
+++ bareminimumplotter.h
@@ -0,0 +1,36 @@
+#ifndef BAREMINIMUMPLOTTER_H
+#define BAREMINIMUMPLOTTER_H
+
+#include <QMainWindow>
+#include <vector>
+#include "deplot/include/variable.h"
+#include "deplot/include/inequality.h"
+#include "deplot/include/expression.h"
+
+namespace Ui {
+class BareMinimumPlotter;
+}
+
+class BareMinimumPlotter : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit BareMinimumPlotter(QWidget *parent = 0);
+    ~BareMinimumPlotter();
+
+    void plot();
+    bool isEmpty_InputFields();
+
+private slots:
+    void on_buttonPlot_clicked();
+
+private:
+    Ui::BareMinimumPlotter *ui;
+    Variable mVariable1, mVariable2;
+    Expression mExpression1, mExpression2;
+    Inequality mInequality;
+
+};
+
+#endif // BAREMINIMUMPLOTTER_H
